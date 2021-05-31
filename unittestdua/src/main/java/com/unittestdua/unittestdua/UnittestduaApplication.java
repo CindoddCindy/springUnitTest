e7a4d8e8ede247147aff0ba894a3ada7900d04aa
CindoddCindy <cindodcindy@gmail.com>
2021-05-31 15:55:59 +0700

    tambah package

---
+++ unittestdua/src/main/java/com/unittestdua/unittestdua/UnittestduaApplication.java
@@ -0,0 +1,13 @@
+package com.unittestdua.unittestdua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnittestduaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnittestduaApplication.class, args);
+	}
+
+}
---
+++ unittestdua/src/test/java/com/unittestdua/unittestdua/UnittestduaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unittestdua.unittestdua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnittestduaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
